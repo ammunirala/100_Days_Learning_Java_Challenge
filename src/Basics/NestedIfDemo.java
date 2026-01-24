3922838c182e237420cb17f8793a0d5ab1fa4ebd
Amresh <138843165+ammunirala@users.noreply.github.com>
2026-01-24 14:46:14 +0530

    Basics of java

---
+++ src/Basics/NestedIfDemo.java
@@ -0,0 +1,17 @@
+package Basics;
+// NestedIf
+
+public class NestedIfDemo {
+    public static void main(String args[]){
+        int i = 50;
+        if (i == 50){
+            System.out.println("i is 50");
+            if (i < 75){
+                System.out.println(" i is smaller than 75");
+                if(i < 55){
+                    System.out.println("i is aslo smaller tham 55");
+                }
+            }
+        }
+    }
+}
